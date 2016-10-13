b45fe64eb6b09770a55912cba53facadc6ca88a2
Andre Miras <andre.miras@gmail.com>
2016-10-14 00:00:58 +0200

    Update list_targets.py decode & print response

--- list_targets.py
+++ list_targets.py
@@ -10,18 +10,38 @@ from pn532_reader import Pn532Reader
 
 
 def run(pn532_reader):
-    COMMAND_STR = "D4 32 01 00"
-    response_str = pn532_reader.send_apdu_str(COMMAND_STR)
-    print("response_str: %s" % response_str)
+    command_str = "D4 32 01 00"
+    response_str = pn532_reader.send_apdu_str(command_str)
+    # print("response_str: %s" % response_str)
 
-    COMMAND_STR = "D4 32 01 01"
-    response_str = pn532_reader.send_apdu_str(COMMAND_STR)
-    print("response_str: %s" % response_str)
+    command_str = "D4 32 01 01"
+    response_str = pn532_reader.send_apdu_str(command_str)
+    # print("response_str: %s" % response_str)
 
     # 4A InListPassivTargets
-    COMMAND_STR = "D4 4A 01 00"
-    response_str = pn532_reader.send_apdu_str(COMMAND_STR)
-    print("response_str: %s" % response_str)
+    command_str = "D4 4A 01 00"
+    response_str = pn532_reader.send_apdu_str(command_str)
+    response = [int(x, 16) for x in response_str.split()]
+    print("response: %s" % response_str)
+    response_header = response[0:2]
+    response_header_str = "%02X %02X" % (
+        response_header[0], response_header[1])
+    tags_found = response[2]
+    target_number = response[3]
+    sens_res = response[4:6]
+    sens_res_str = "%02X %02X" % (
+        sens_res[0], sens_res[1])
+    sel_res = response[6]
+    uid_length = response[7]
+    uid = response[8:8+uid_length]
+    uid_str = " ".join(["%02X" % x for x in uid])
+    print("response_header:", response_header_str)
+    print("tags_found:", tags_found)
+    print("target_number:", target_number)
+    print("sens_res:", sens_res_str)
+    print("sel_res:", sel_res)
+    print("uid_length:", uid_length)
+    print("uid:", uid_str)
 
 
 def main():
@@ -31,7 +51,7 @@ def main():
     reader = pcsc_readers[0]
     print("Using reader: %s" % reader)
     pn532_reader = Pn532Reader(reader)
-    pn532_reader._debug = True
+    pn532_reader._debug = False
     run(pn532_reader)
 
 if __name__ == "__main__":
