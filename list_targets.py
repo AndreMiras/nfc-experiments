3b3119178166fabf321db9faca0a598b70c51745
Andre Miras <andre.miras@gmail.com>
2016-10-14 23:47:19 +0200

    Update list_targets.py
    Inventory on all supported modulations.

--- list_targets.py
+++ list_targets.py
@@ -69,18 +69,20 @@ def decode_target_data(target_data, nfc_modulation):
     elif nfc_modulation == NfcModulation.NMT_ISO14443B:
         decode_target_data_iso14443b(target_data)
     else:
-        raise NotImplemented('')
+        raise NotImplementedError('')
 
 
 def pn_to_nfc_modulation(pn532_modulation):
-    if pn532_modulation == Pn532Modulation.PM_ISO14443A_106.value:
+    if pn532_modulation == Pn532Modulation.PM_ISO14443A_106:
         return NfcModulation.NMT_ISO14443A
-    elif pn532_modulation == Pn532Modulation.PM_ISO14443B_106.value:
+    elif pn532_modulation == Pn532Modulation.PM_ISO14443B_106:
         return NfcModulation.NMT_ISO14443B
-    raise NotImplemented('')
+    raise NotImplementedError(
+        '%s modulation not implemented' % pn532_modulation)
 
 
-def run(pn532_reader):
+def list_target(pn532_reader, pn532_modulation):
+    print("pn532_modulation:", pn532_modulation)
     # Turns on the antenna power
     # 0x32 RFConfiguration
     command_str = "32 01 01"
@@ -90,24 +92,33 @@ def run(pn532_reader):
     command_str = "32 05 00 00 00"
     response_str = pn532_reader.send_apdu_str(command_str)
 
-    pn532_modulation = Pn532Modulation.PM_ISO14443A_106.value
-    # pn532_modulation = Pn532Modulation.PM_ISO14443B_106.value
     # 0x4A InListPassivTargets
-    command_str = "4A 01 %02X" % (pn532_modulation)
+    command_str = "4A 01 %02X" % (pn532_modulation.value)
     # command_str = "4A 01 %02X 00" % (Pn532Modulation.PM_ISO14443B_106.value)
-    if pn532_modulation == Pn532Modulation.PM_ISO14443B_106.value:
+    if pn532_modulation == Pn532Modulation.PM_ISO14443B_106:
         # optional initiator data (used for Felica, ISO14443B,
         # Topaz Polling or for ISO14443A selecting a specific UID)
         command_str += " 00"
     response_str = pn532_reader.send_apdu_str(command_str)
     response = [int(x, 16) for x in response_str.split()]
-    print("response: %s" % response_str)
     nfc_modulation = pn_to_nfc_modulation(pn532_modulation)
     decode_target_data(response, nfc_modulation)
     # Turns off the antenna power
     # 0x32 RFConfiguration
     command_str = "32 01 00"
     response_str = pn532_reader.send_apdu_str(command_str)
+
+
+def list_targets(pn532_reader):
+    """
+    List targets for all supported modulations.
+    """
+    pn532_modulations = [
+        Pn532Modulation.PM_ISO14443A_106,
+        Pn532Modulation.PM_ISO14443B_106,
+    ]
+    for pn532_modulation in pn532_modulations:
+        list_target(pn532_reader, pn532_modulation)
 
 
 def main():
@@ -118,7 +129,7 @@ def main():
     print("Using reader: %s" % reader)
     pn532_reader = Pn532Reader(reader)
     pn532_reader._debug = False
-    run(pn532_reader)
+    list_targets(pn532_reader)
 
 if __name__ == "__main__":
     main()
